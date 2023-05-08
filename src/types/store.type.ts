c044298ee58b4499870d5551e5e57ec3c78bfc2e
PriyaVeruva <priyaveruva55@gmail.com>
2023-05-08 21:31:32 +0530

    updated import file in store.types.ts

--- src/types/store.type.ts
+++ src/types/store.type.ts
@@ -1,4 +1,4 @@
-import { SignUpState } from '../redux/slices/authslice';
+import { SignUpState } from 'redux/slices/auth.types';
 import { ToolkitStore } from '@reduxjs/toolkit/dist/configureStore';
 import { AnyAction, EmptyObject } from 'redux';
 import { Persistor } from 'redux-persist';
